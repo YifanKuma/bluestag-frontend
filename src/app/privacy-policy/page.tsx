6054f16ed1edaba64690443fc3a0ce27de3a8921
YifanKuma <yifan.huang0219@gmail.com>
2025-12-02 16:23:12 +1000

    Fix privacy policy dynamic fetch and revalidate

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,6 @@
+export const revalidate = 60;
+// OR: export const dynamic = "force-dynamic";
+
 import type {Metadata} from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
