6b2c60786a9d71f7b544d122ef9f68fc18fc64e4
YifanKuma <yifan.huang0219@gmail.com>
2025-12-04 14:55:28 +1000

    Connect frontend to online Strapi

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer";
-
 import PricingPageClient from "@/app/pricing/PricingPageClient";
 import {getPricingPage} from "@/lib/strapi";
 import NavbarServer from "@/components/NavbarServer";
@@ -8,14 +7,26 @@ export const dynamic = "force-dynamic";
 
 export default async function PricingPageWrapper() {
     const data = await getPricingPage();
+
     if (!data) {
         return (
             <main className="min-h-screen text-white pt-16">
-                <NavbarServer />
+                <NavbarServer/>
                 <div className="p-12">Failed to load pricing data.</div>
+                <Footer/>
             </main>
         );
     }
 
-    return <PricingPageClient data={data}/>;
+    return (
+        <main className="relative min-h-screen text-white pt-16">
+            <NavbarServer/>
+
+            <section id="pricing">
+                <PricingPageClient data={data}/>
+            </section>
+
+            <Footer/>
+        </main>
+    );
 }
