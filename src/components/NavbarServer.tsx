0a02809149ffe3820b0fee0056ba7a283304df3e
YifanKuma <yifan.huang0219@gmail.com>
2025-12-04 16:41:22 +1000

    Connect frontend to online Strapi

--- src/components/NavbarServer.tsx
+++ src/components/NavbarServer.tsx
@@ -1,8 +1,12 @@
-import { getNavbar } from "@/lib/strapi";
+import {getNavbar} from "@/lib/strapi";
 import NavbarClient from "./NavbarClient";
 
 export default async function NavbarServer() {
     const data = await getNavbar();
+
+    console.log("NavbarServer → STRAPI_URL =", process.env.NEXT_PUBLIC_STRAPI_URL);
+    console.log("NavbarServer → STRAPI_TOKEN exists =", !!process.env.STRAPI_TOKEN);
+
 
     if (!data) return null;
 
@@ -11,5 +15,5 @@ export default async function NavbarServer() {
         ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${data.logo.url}`
         : null;
 
-    return <NavbarClient navbar={{ ...data, logoUrl }} />;
+    return <NavbarClient navbar={{...data, logoUrl}}/>;
 }
